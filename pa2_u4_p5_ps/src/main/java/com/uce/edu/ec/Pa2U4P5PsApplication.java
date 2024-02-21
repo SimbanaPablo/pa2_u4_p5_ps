08862cebd5ba41762bdf49781059f262fbbb7c26
SimbanaPablo <pablo.simbana01@gmail.com>
2024-02-20 20:31:03 -0500

    main

---
+++ pa2_u4_p5_ps/src/main/java/com/uce/edu/ec/Pa2U4P5PsApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P5PsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P5PsApplication.class, args);
+	}
+
+}
---
+++ pa2_u4_p5_ps/src/test/java/com/uce/edu/ec/Pa2U4P5PsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U4P5PsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
